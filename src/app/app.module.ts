32c0afa1e33e367fa66d633ebce0ec762e3ca919
Larry Hatcer <actswolf@gmail.com>
2022-01-30 18:49:42 -0600

    Got the toggle to show the form working with color change.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
@@ -11,7 +12,9 @@ import { TasksComponent } from './componets/tasks/tasks.component';
 import { TasksItemComponent } from './componets/tasks-item/tasks-item.component';
 import { AddTaskComponent } from './componets/add-task/add-task.component';
 
-
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
